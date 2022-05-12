6a3613dbae9ed279f3ad32005269727ad3c1fa4e
tanlang <lin.zexiao@qq.com>
2022-05-12 14:38:27 +0800

    fix: nettype jundge in compose.sh error

--- docker/script/compose.sh
+++ docker/script/compose.sh
@@ -4,14 +4,19 @@ echo $@
 
 token=$(cat /env/token )
 
+echo "token:"
 echo ${token}
-echo ${nettype}
 
-if [ $nettype=="cali" ]
-then
+
+if [ $nettype = "cali" ];then
     nettype="calibnet"
 fi
-    echo $nettype
+
+echo "nettype:"
+echo $nettype
+
+
+    
 
 /app/venus-miner init --nettype ${nettype} --auth-api http://127.0.0.1:8989 --token ${token} --gateway-api /ip4/127.0.0.1/tcp/45132 --api /ip4/127.0.0.1/tcp/3453 --slash-filter local
 
