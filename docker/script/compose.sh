b96fd6185731b359c1dc51d4790b1796a4cdf75b
tanlang <lin.zexiao@qq.com>
2022-06-28 17:47:47 +0800

    fix: avoid reinit when miner relaunch

--- docker/script/compose.sh
+++ docker/script/compose.sh
@@ -15,9 +15,12 @@ fi
 echo "nettype:"
 echo $nettype
 
+if [ ! -d "/root/.venusminer" ];
+then
+    echo "not found ~/.venusminer"
+    /app/venus-miner init --nettype ${nettype} --auth-api http://127.0.0.1:8989 --token ${token} --gateway-api /ip4/127.0.0.1/tcp/45132 --api /ip4/127.0.0.1/tcp/3453 --slash-filter local
+fi
 
-    
 
-/app/venus-miner init --nettype ${nettype} --auth-api http://127.0.0.1:8989 --token ${token} --gateway-api /ip4/127.0.0.1/tcp/45132 --api /ip4/127.0.0.1/tcp/3453 --slash-filter local
 
 /app/venus-miner run --nettype ${nettype}
