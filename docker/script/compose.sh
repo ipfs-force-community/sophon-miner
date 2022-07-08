964ef75dc86be4d193041c37d46df14d296e258e
Mike <41407352+hunjixin@users.noreply.github.com>
2022-07-08 11:16:11 +0800

    Merge pull request #112 from filecoin-project/chore/tanlang/ignore-nettype-arg
    fix: ignore nettype arg in docker compoes script

--- docker/script/compose.sh
+++ docker/script/compose.sh
@@ -8,19 +8,12 @@ echo "token:"
 echo ${token}
 
 
-if [ $nettype = "cali" ];then
-    nettype="calibnet"
-fi
-
-echo "nettype:"
-echo $nettype
-
 if [ ! -d "/root/.venusminer" ];
 then
     echo "not found ~/.venusminer"
-    /app/venus-miner init --nettype ${nettype} --auth-api http://127.0.0.1:8989 --token ${token} --gateway-api /ip4/127.0.0.1/tcp/45132 --api /ip4/127.0.0.1/tcp/3453 --slash-filter local
+    /app/venus-miner init  --auth-api http://127.0.0.1:8989 --token ${token} --gateway-api /ip4/127.0.0.1/tcp/45132 --api /ip4/127.0.0.1/tcp/3453 --slash-filter local
 fi
 
 
 
-/app/venus-miner run --nettype ${nettype}
+/app/venus-miner run 
