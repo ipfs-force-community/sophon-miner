4a38d0a5cf887c2539a837621e33d2a8d80958a1
zl <zl03jsj@163.com>
2022-08-09 17:25:00 +0800

    refactor: remove local declear of `KeyType`, use the one defined in venus-shared

--- types/keystore.go
+++ types/keystore.go
@@ -1,76 +1,15 @@
 package types
 
-import (
-	"encoding/json"
-	"fmt"
-
-	logging "github.com/ipfs/go-log/v2"
-
-	"github.com/filecoin-project/go-state-types/crypto"
-)
-
-var log = logging.Logger("types")
-
-var (
-	ErrKeyInfoNotFound = fmt.Errorf("key info not found")
-	ErrKeyExists       = fmt.Errorf("key already exists")
-)
-
-// KeyType defines a type of a key
-type KeyType string
-
-func (kt *KeyType) UnmarshalJSON(bb []byte) error {
-	{
-		// first option, try unmarshaling as string
-		var s string
-		err := json.Unmarshal(bb, &s)
-		if err == nil {
-			*kt = KeyType(s)
-			return nil
-		}
-	}
-
-	{
-		var b byte
-		err := json.Unmarshal(bb, &b)
-		if err != nil {
-			return fmt.Errorf("could not unmarshal KeyType either as string nor integer: %w", err)
-		}
-		bst := crypto.SigType(b)
-
-		switch bst {
-		case crypto.SigTypeBLS:
-			*kt = KTBLS
-		case crypto.SigTypeSecp256k1:
-			*kt = KTSecp256k1
-		default:
-			return fmt.Errorf("unknown sigtype: %d", bst)
-		}
-		log.Warnf("deprecation: integer style 'KeyType' is deprecated, switch to string style")
-		return nil
-	}
-}
-
-const (
-	KTBLS             KeyType = "bls"
-	KTSecp256k1       KeyType = "secp256k1"
-	KTSecp256k1Ledger KeyType = "secp256k1-ledger"
-)
-
-// KeyInfo is used for storing keys in KeyStore
-type KeyInfo struct {
-	Type       KeyType
-	PrivateKey []byte
-}
+import "github.com/filecoin-project/venus/venus-shared/types"
 
 // KeyStore is used for storing secret keys
 type KeyStore interface {
 	// List lists all the keys stored in the KeyStore
 	List() ([]string, error)
 	// Get gets a key out of keystore and returns KeyInfo corresponding to named key
-	Get(string) (KeyInfo, error)
+	Get(string) (types.KeyInfo, error)
 	// Put saves a key info under given name
-	Put(string, KeyInfo) error
+	Put(string, types.KeyInfo) error
 	// Delete removes a key from keystore
 	Delete(string) error
 }
