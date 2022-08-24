dd84e9082db0818a5ea69e3ad5a2a9867c97f299
tanlang <lin.zexiao@qq.com>
2022-08-24 10:12:58 +0800

    feat: use auth client from venus-auth

--- node/impl/common/common.go
+++ node/impl/common/common.go
@@ -2,16 +2,11 @@ package common
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/filecoin-project/venus-miner/types"
-
-	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/google/uuid"
 	logging "github.com/ipfs/go-log/v2"
 	"go.uber.org/fx"
-
-	"github.com/filecoin-project/go-jsonrpc/auth"
 
 	"github.com/filecoin-project/venus-miner/api"
 	"github.com/filecoin-project/venus-miner/build"
@@ -23,29 +18,7 @@ var session = uuid.New()
 type CommonAPI struct {
 	fx.In
 
-	APISecret    *types.APIAlg
 	ShutdownChan types.ShutdownChan
-}
-
-type jwtPayload struct {
-	Allow []auth.Permission
-}
-
-func (a *CommonAPI) AuthVerify(ctx context.Context, token string) ([]auth.Permission, error) {
-	var payload jwtPayload
-	if _, err := jwt.Verify([]byte(token), (*jwt.HMACSHA)(a.APISecret), &payload); err != nil {
-		return nil, fmt.Errorf("JWT Verification failed: %w", err)
-	}
-
-	return payload.Allow, nil
-}
-
-func (a *CommonAPI) AuthNew(ctx context.Context, perms []auth.Permission) ([]byte, error) {
-	p := jwtPayload{
-		Allow: perms, // TODO: consider checking validity
-	}
-
-	return jwt.Sign(&p, (*jwt.HMACSHA)(a.APISecret))
 }
 
 var apiVersion = vapi.NewVer(1, 2, 0)
