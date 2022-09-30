3aff9987f729b1c81962d62fc270f1e3bb08ae52
一页素书 <2931107265@qq.com>
2022-09-30 17:39:35 +0800

    chore: upgrade version

--- build/version.go
+++ build/version.go
@@ -3,7 +3,7 @@ package build
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.7.0"
+const BuildVersion = "1.7.1"
 
 func UserVersion() string {
 	return BuildVersion + CurrentCommit
