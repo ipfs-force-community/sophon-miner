0d6887a629ae99b5bde6f3c6222d6c6746359e62
一页素书 <2931107265@qq.com>
2022-11-16 16:09:35 +0800

    chore: bump version to 1.8.0

--- build/version.go
+++ build/version.go
@@ -3,7 +3,7 @@ package build
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.8.0-rc5"
+const BuildVersion = "1.8.0"
 const Version = "180"
 
 func UserVersion() string {
