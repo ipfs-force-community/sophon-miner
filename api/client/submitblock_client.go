8efe5ec880461a1b46246c3ca87ba0d5bb2177d9
Tiance <1033935631@qq.com>
2022-12-09 14:36:17 +0800

    feat: submit block to more node

---
+++ api/client/submitblock_client.go
@@ -0,0 +1,21 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/filecoin-project/go-jsonrpc"
+
+	"github.com/filecoin-project/venus-miner/node/config"
+
+	v1 "github.com/filecoin-project/venus/venus-shared/api/chain/v1"
+)
+
+func NewSubmitBlockRPC(ctx context.Context, apiInfo *config.APIInfo) (v1.FullNode, jsonrpc.ClientCloser, error) {
+	addr, err := apiInfo.DialArgs("v1")
+	if err != nil {
+		return nil, nil, fmt.Errorf("could not get DialArgs: %w", err)
+	}
+
+	return v1.NewFullNodeRPC(ctx, addr, apiInfo.AuthHeader())
+}
