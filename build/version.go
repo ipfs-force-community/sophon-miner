cd51a7dd32bb4c18fbbd63de59c52f103d55b035
Tiance <1033935631@qq.com>
2022-12-09 15:42:11 +0800

    chore: bump version to 1.8.1

--- build/version.go
+++ build/version.go
@@ -3,8 +3,8 @@ package build
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.8.0"
-const Version = "180"
+const BuildVersion = "1.8.1"
+const Version = "181"
 
 func UserVersion() string {
 	return BuildVersion + CurrentCommit
