4dcdcdb143f4dd8bc9feaf7547f79063cec1d28e
Tiance <1033935631@qq.com>
2023-04-11 11:23:59 +0800

    chore: bump version to 1.10.2

--- build/version.go
+++ build/version.go
@@ -3,7 +3,7 @@ package build
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.10.1"
+const BuildVersion = "1.10.2"
 const Version = "1100"
 
 func UserVersion() string {
