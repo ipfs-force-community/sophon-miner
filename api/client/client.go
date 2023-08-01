e5fd969af458d95b834d38db8485e9d452a627b4
tanlang <lin.zexiao@qq.com>
2023-08-01 13:23:17 +0800

    feat: add  venus name space header

--- api/client/client.go
+++ api/client/client.go
@@ -6,13 +6,18 @@ import (
 
 	"github.com/filecoin-project/go-jsonrpc"
 
+	venus_api "github.com/filecoin-project/venus/venus-shared/api"
 	"github.com/ipfs-force-community/sophon-miner/api"
 )
+
+const MajorVersion = 0
+const APINamespace = "miner.MinerAPI"
+const MethodNamespace = "Filecoin"
 
 // NewCommonRPC creates a new http jsonrpc client.
 func NewCommonRPC(ctx context.Context, addr string, requestHeader http.Header) (api.Common, jsonrpc.ClientCloser, error) {
 	var res api.CommonStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, MethodNamespace,
 		[]interface{}{
 			&res.Internal,
 		},
@@ -24,8 +29,9 @@ func NewCommonRPC(ctx context.Context, addr string, requestHeader http.Header) (
 
 // NewMinerRPC creates a new http jsonrpc client for miner
 func NewMinerRPC(ctx context.Context, addr string, requestHeader http.Header, opts ...jsonrpc.Option) (api.MinerAPI, jsonrpc.ClientCloser, error) {
+	requestHeader.Set(venus_api.VenusAPINamespaceHeader, APINamespace)
 	var res api.MinerAPIStruct
-	closer, err := jsonrpc.NewMergeClient(ctx, addr, "Filecoin",
+	closer, err := jsonrpc.NewMergeClient(ctx, addr, MethodNamespace,
 		[]interface{}{
 			&res.CommonStruct.Internal,
 			&res.Internal,
