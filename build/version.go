a95d4c8743a57415a1b9167401c06d36a43ed6c0
simlecode <69969590+simlecode@users.noreply.github.com>
2023-09-21 09:52:17 +0800

    chore: bump version to v1.13.1

--- build/version.go
+++ build/version.go
@@ -3,8 +3,8 @@ package build
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.13.0"
-const Version = "1130"
+const BuildVersion = "1.13.1"
+const Version = "1131"
 
 func UserVersion() string {
 	return BuildVersion + CurrentCommit
