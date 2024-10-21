1a37ffdf6cc093bb93fd48037842eddeb3663be8
vladmair <vladmairwei@gmail.com>
2024-10-21 16:06:27 +0800

    chore: bump version to 1.17.0-rc1

--- build/version.go
+++ build/version.go
@@ -3,7 +3,7 @@ package build
 var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
-const BuildVersion = "1.16.0"
+const BuildVersion = "1.17.0-rc1"
 const Version = "1160"
 
 func UserVersion() string {
