01593c2df1b77789e9f7938fbd71ce167ad9099c
vladmair <vladmairwei@gmail.com>
2024-10-21 16:13:24 +0800

    update version

--- build/version.go
+++ build/version.go
@@ -4,7 +4,7 @@ var CurrentCommit string
 
 // BuildVersion is the local build version, set by build system
 const BuildVersion = "1.17.0-rc1"
-const Version = "1160"
+const Version = "1170"
 
 func UserVersion() string {
 	return BuildVersion + CurrentCommit
